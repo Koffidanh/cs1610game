cac6ba479a244eaf2522d5c76673cb5fc7dc6564
Zach Allred <u0607562@utah.edu>
2023-09-21 09:09:22 -0600

    add collisions

---
+++ src/Collisions.cpp
@@ -0,0 +1,8 @@
+//
+//  Collisions.cpp
+//  carracing
+//
+//  Created by Zach Allred on 9/20/23.
+//
+
+#include "Collisions.hpp"
---
+++ src/Collisions.hpp
@@ -0,0 +1,13 @@
+//
+//  Collisions.hpp
+//  carracing
+//
+//  Created by Zach Allred on 9/20/23.
+//
+
+#ifndef Collisions_hpp
+#define Collisions_hpp
+
+#include <stdio.h>
+
+#endif /* Collisions_hpp */
